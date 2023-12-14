00f6e4e43addea32ddca55a7c34b8823de00f7bd
Pedro Rocha <rocha.pedroaf@gmail.com>
2023-12-14 12:38:16 -0300

    Creates product cards from the product data

---
+++ backend/src/controllers/ListCustomersController.ts
@@ -0,0 +1,15 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+
+import { ListCustomersService } from "../services/ListCustomersService";
+
+class ListCustomerController {
+  async handle(request: FastifyRequest, reply: FastifyReply) {
+    const listCustomersService = new ListCustomersService();
+
+    const customers = await listCustomersService.execute();
+
+    reply.send(customers);
+  }
+}
+
+export { ListCustomerController };
--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+// ALERTA DE ITENS NO CARRINHO NO MENU
 
+const cartItems = [1, 2];
+
+const cartAmountAlert = document.querySelector(".cart-amount");
+
+cartAmountAlert.addEventListener("click", () => {
+  console.log("test");
+});
+
+cartAmountAlert.textContent = cartItems.length;
